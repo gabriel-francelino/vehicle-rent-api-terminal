ca27021a4ba5ec96d55817711e6ba1e663b37a32
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-11-26 02:48:13 -0300

    primeira versão da interface criada

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,89 @@
-// Importa o módulo prompt-sync
 import * as promptSync from 'prompt-sync';
+import { Interface } from './interface';
 
-function main() {
-    // Cria uma instância do prompt-sync
-    const prompt = promptSync();
+const prompt = promptSync();
+let option: string = "99";
 
-    // Solicita o nome do usuário
-    const nome: string = prompt('Qual é o seu nome? ');
+try {
+    do {
+        Interface.show();
+        option = prompt("Escolha uma opção: ");
+        switch (option) {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            case "6":
+                break;
+            case "7":
+                break;
+            case "8":
+                break;
+            case "9":
+                break;
+            case "10":
+                break;
+            case "11":
+                break;
+            case "12":
+                break;
+            case "13":
+                break;
+            case "0":
+                console.log("Saindo...");
+                console.log(option)
+                break;
+            default:
+                break;
+        }
 
-    // Imprime o nome na tela
-    console.log(`Olá, ${nome}!`);
+        // if(option == "0") {
+        //     break;
+        // }
+    } while (option !== "0");
+} catch (error) {
+    
 }
 
-// Chama a função principal
-main();
+
+
+// function main() {
+// }
+
+// // Chama a função principal
+// main();
+
+// const customer1 = new Customer("12345678910", "João", new Date("1990-01-01"), ECategoryType.A);
+// const customer2 = new Customer("12345678911", "Maria", new Date("1990-01-01"), ECategoryType.B);
+// const customer3 = new Customer("12345678912", "José", new Date("1990-01-01"), ECategoryType.C);
+
+// Customer.create(customer1);
+// Customer.create(customer2);
+// Customer.create(customer3);
+
+// const vehicle1 = new Vehicle("Gol", "Preto", "AAAAAA", "CAR", "ABC-123", 200);
+// const vehicle2 = new Vehicle("Uno", "Branco", "BBBBBB", "CAR", "DEF-456", 300);
+// const vehicle3 = new Vehicle("Fiesta", "Vermelho", "CCCCCC", "CAR", "GHI-789", 400);
+
+// Vehicle.create(vehicle1);
+// Vehicle.create(vehicle2);
+// Vehicle.create(vehicle3);
+
+// const rent1 = new Rent(customer1, vehicle1, new Date("2021-01-01"), new Date("2021-01-02"));
+// const rent2 = new Rent(customer2, vehicle2, new Date("2021-01-01"), new Date("2021-01-02"));
+// const rent3 = new Rent(customer3, vehicle3, new Date("2021-01-01"), new Date("2021-01-02"));
+
+// const newRent = Rent.rentVehicle("12345678910", "ABC-123", new Date("2023-11-26"), new Date("2023-11-30"));
+// console.log(newRent);
+// const invoice = Rent.generateInvoice("12345678910", "ABC-123");
+// console.log(invoice);
+
+// const devRent = Rent.devolutionVehicle("12345678910", "ABC-123");
+// console.log(devRent);
+// console.log(Rent.listOfRents);
---
+++ src/interface.ts
@@ -0,0 +1,23 @@
+export class Interface {
+    static show(): void {
+        console.log(`
+        ==============================
+        Escolha uma opção:
+        ------------------------------
+        1  | Cadastrar cliente
+        2  | Remover cliente
+        3  | Listar clientes
+        4  | Listar clientes por cpf
+        5  | Cadastrar veículo
+        6  | Remover veículo
+        7  | Listar veículos
+        8  | Listar veículos por placa
+        9  | Listar veículos alugados
+        10 | Listar veículos disponíveis
+        11 | Alugar veículo
+        12 | Devolver veículo
+        13 | Gerar fatura do aluguel
+        ==============================
+        `)
+    }
+}
